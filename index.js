95784947cbfce97684a4dfac8bd2aaa16c983948
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2024-08-11 18:14:02 +0530

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const { connection } = require("./config/connection");
+const { routers } = require("./router/router");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use("", routers);
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log(`sercver connected with port ${process.env.PORT}`);
+  } catch (error) {
+    console.log("connection failed");
+  }
+});
